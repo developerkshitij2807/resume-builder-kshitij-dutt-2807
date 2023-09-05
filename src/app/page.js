d3bcb6b1f51df52a7df4ecea6746b3cae926f83b
Kshitij Dutt <kshtjdutt07@gmail.com>
2023-09-05 19:32:54 +0530

    "use client" field added

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,19 @@
+"use client";
+
 import { ApolloProvider } from "@apollo/client";
 
+
+
 import client from "@/lib/apollo-client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <ApolloProvider client={client}>
       Next JS with Apollo Graphql + MongoDB
       <p>Proceed to /api/graphql to checkout the playgorund</p>
-      <button>click</button>
+      <button onClick={() => router.push("/api/graphql")}>click</button>
     </ApolloProvider>
   );
 }
