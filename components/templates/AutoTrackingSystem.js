e201ed92b7b159aed9beee22f7dc3a2f57e419cc
KshitijDutt Patchinfotech <kshitij.dutt@patchinfotech.com>
2023-09-09 19:09:15 +0530

    ATS template

---
+++ components/templates/AutoTrackingSystem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AutoTrackingSystemTemplate = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">This is a Template for the page Auto Tracking System.</h1>
+  )
+}
+
+export default AutoTrackingSystemTemplate
---
+++ pages/templates/ATS.js
@@ -0,0 +1,13 @@
+import AutoTrackingSystemTemplate from '@/components/templates/AutoTrackingSystem'
+import React from 'react'
+
+const ATS = () => {
+  return (
+    <>
+    <title>Auto Tracking System Page</title>
+    <AutoTrackingSystemTemplate />
+    </>
+  )
+}
+
+export default ATS
