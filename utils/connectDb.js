cc64937bc1cb4923bf14e06ddafdd261f9b4c675
KshitijDutt Patchinfotech <kshitij.dutt@patchinfotech.com>
2023-09-13 21:07:28 +0530

    db connected

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,24 +1,22 @@
 import mongoose from "mongoose";
 // define MongoDB Url in the environment variables
-const localUri = process.env.DATABASE_URL.replace("<username>", process.env.DATABASE_USER).replace("<password>", process.env.DATABASE_PASSWORD);
+const localUri = process.env.DATABASE_URL.replace("<username>", process.env.DATABASE_USERNAME).replace("<password>", process.env.DATABASE_PASSWORD);
+
 const connection = {};
 export async function connectDb() {
   if (connection.isConnected) {
-    console.log("DB is already connected");
     return;
   }
 
   if (mongoose.connections.length > 0) {
     connection.isConnected = mongoose.connections[0].readyState;
     if (connection.isConnected === 1) {
-      console.log("use previous connection");
       return;
     }
     await mongoose.disconnect();
   }
 
   const db = await mongoose.connect(localUri);
-  console.log("? MongoDB Database Connected Successfully");
   connection.isConnected = db.connections[0].readyState;
 }
 
