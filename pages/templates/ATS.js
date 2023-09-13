0c741de15a6b418517449366b68461ebf28a352b
KshitijDutt Patchinfotech <kshitij.dutt@patchinfotech.com>
2023-09-13 21:07:16 +0530

    ATS template configured

--- pages/templates/ATS.js
+++ pages/templates/ATS.js
@@ -1,13 +1,14 @@
-
 import React from 'react'
 import AutoTrackingSystemTemplate from '@/src/components/templates/AutoTrackingSystem'
+import { Providers } from 'providers'
+
 
 const ATS = () => {
   return (
-    <>
+    <Providers>
       <title>Auto Tracking System Page</title>
       <AutoTrackingSystemTemplate />
-    </>
+    </Providers>
   )
 }
 
--- src/components/templates/AutoTrackingSystem.js
+++ src/components/templates/AutoTrackingSystem.js
@@ -1,13 +1,21 @@
+"use client";
+
 import React from 'react'
 import { CenterAlignBox } from '@/src/components/custom/CenterAlignBox';
+import { GET_USER_DETAILS_QUERY } from '@/graphql/queries/userQueries';
+import { useQuery } from '@apollo/client';
+
 
 
 
 const AutoTrackingSystemTemplate = () => {
-  const firstName = "Kshitij";
-  const secondName = "Dutt"
+
+  const { data } = useQuery(GET_USER_DETAILS_QUERY);
+
+  console.log(data);
+
   return (
-    <CenterAlignBox>{firstName + secondName}</CenterAlignBox>
+    <CenterAlignBox></CenterAlignBox>
   )
 }
 
